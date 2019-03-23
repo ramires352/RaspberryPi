3b42ff5926be3491953836e9100772b53db811d7
Raul Ramires <ramires1432@outlook.com>
2019-03-23 17:59:15 -0300

    Exemplo de argumentos em linha de comando

---
+++ arguments.py
@@ -0,0 +1,20 @@
+import sys, getopt
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hp:m:")
+    except getopt.GetoptError:
+        print("arguments.py -p <pin> -m <message>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == "-h":
+            print("arguments.py -p <pin> -m <message>")
+            sys.exit()
+        elif opt == "-p":
+            pino = arg
+        elif opt == "-m":
+            mensagem = arg
+    print(pino, mensagem)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
